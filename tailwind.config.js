945a5fd2b4d1194ce6f5e08357a6ff2f20f4110c
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2022-04-19 15:24:41 +0200

    Added more breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
             "bright-cyan": "hsl(192, 70%, 51%)",
             
             // Neutral
-            "grayish-Blue": "hsl(233, 8%, 62%)",
+            "grayish-blue": "hsl(233, 8%, 62%)",
             "light-grayish-blue": "hsl(220, 16%, 96%)",
             "very-light-gray": "hsl(0, 0%, 98%)",
             "white": "hsl(0, 0%, 100%)"
@@ -20,7 +20,13 @@ module.exports = {
             "main": ["Public Sans", "sans-serif"]
         },
         screens: {
-            "md": "1024px"
+            "ssm": "600px",
+            "sm": "768px",
+            "md": "1024px",
+            "mlg": "1200px",
+            "lg": "1440px",
+            "xl": "1600px",
+            "xxl": "1800px"
         }
     },
     plugins: [],
