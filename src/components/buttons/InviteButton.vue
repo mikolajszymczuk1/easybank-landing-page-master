411b0af817f3cdd48a399782810239b1734ad8ef
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2022-04-19 02:01:36 +0200

    Create InviteButton component

---
+++ src/components/buttons/InviteButton.vue
@@ -0,0 +1,7 @@
+<template>
+    <a class="bg-gradient-to-r from-lime-green to-bright-cyan rounded-[25px] px-[35px] py-[11px] font-main font-bold text-[0.85em] text-white/95"
+        href="#RequestInvite"
+    >
+        Request Invite
+    </a>
+</template>
