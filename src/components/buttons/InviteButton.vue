fd58d29add0eeab30625f9ba79b1a2452d6ccde8
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2022-04-19 15:27:00 +0200

    Add inline-block property

--- src/components/buttons/InviteButton.vue
+++ src/components/buttons/InviteButton.vue
@@ -1,5 +1,5 @@
 <template>
-    <a class="bg-gradient-to-r from-lime-green to-bright-cyan rounded-[25px] px-[35px] py-[11px] font-main font-bold text-[0.85em] text-white/95"
+    <a class="inline-block bg-gradient-to-r from-lime-green to-bright-cyan rounded-[25px] px-[35px] py-[11px] font-main font-bold text-[0.85em] text-white/95"
         href="#RequestInvite"
     >
         Request Invite
