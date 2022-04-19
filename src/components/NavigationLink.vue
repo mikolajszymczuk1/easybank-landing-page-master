fd2f507537667fee758b48ed32206e7caacfda70
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2022-04-19 02:02:04 +0200

    Finished navigation bar

--- src/components/NavigationLink.vue
+++ src/components/NavigationLink.vue
@@ -1,6 +1,6 @@
 <template>
     <!-- Navigation link component -->
-    <a class="font-main inline-block"
+    <a class="font-main inline-block relative overflow-hidden"
         :href="linkAddress"
         :class="getClasses"
     >
@@ -26,7 +26,7 @@
         computed: {
             getClasses() {
                 let classes = "";
-                classes += this.isMenuLink ? 'text-[1.1em] ' : 'text-[0.9em] ';
+                classes += this.isMenuLink ? 'text-[1.1em] before:hidden md:text-[0.9em] md:text-grayish-blue md:py-[30px] md:hover:text-dark-blue md:before:block before:absolute before:bottom-0 before:w-[100%] before:h-[5px] before:bg-gradient-to-r before:from-lime-green before:to-bright-cyan before:translate-y-[100%] hover:before:translate-y-0 ' : 'text-[0.9em] ';
                 classes += this.isDarkMode ? 'text-white' : 'text-dark-blue';
                 return classes;
             }
